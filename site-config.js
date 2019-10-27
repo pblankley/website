54832df3bcd10054b745f9a1a039d0b668693f92
pblankley <31221512+pblankley@users.noreply.github.com>
2019-10-27 08:34:04 -0400

    add gatsby-universal starter

---
+++ site-config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `Paul Blankley`,
+  siteTitleShort: `Paul`,
+  siteDescription: `Paul's personal site`,
+  siteUrl: `https://pblankley.github.io/website/`,
+  themeColor: `#111111`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: `pablankley`,
+  },
+};
